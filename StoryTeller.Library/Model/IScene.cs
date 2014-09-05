bdd2c8f581ca025c234c9920b5fd81d9f8ab0001
haytham <haytham@outlook.com>
2014-09-04 21:54:17 -0700

    StoryTeller.Library

---
+++ StoryTeller.Library/Model/IScene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryTeller.Library.Model
+{
+    public interface IScene
+    {
+        string Id { get; set; }
+        public SceneType Type { get; set; }
+        ISceneContent Content { get; set; }
+        IScene FollowingScene { get; set; }
+    }
+}
---
+++ StoryTeller.Library/Model/InteractiveScene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryTeller.Library.Model
+{
+    public sealed class InteractiveScene : Scene
+    {
+        public IEnumerable<IScene> PossibleScenes { get; set; }
+    }
+}
