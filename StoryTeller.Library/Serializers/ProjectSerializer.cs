ddd626a3b0d4acca0ad48e31b3eb13098c4eac0e
haytham <haytham@outlook.com>
2014-09-29 17:01:29 -0700

    Prepare Load/Save

---
+++ StoryTeller.Library/Serializers/ProjectSerializer.cs
@@ -0,0 +1,18 @@
+﻿using StoryTeller.DataModel.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryTeller.DataModel.Serializers
+{
+    public sealed class ProjectSerializer
+    {
+        public void Serialize(Stream destination, StoryProject project)
+        {
+
+        }
+    }
+}
